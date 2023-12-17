3f283a4136a4d1112fc5761a9ef2c63485d76104
AllanSoaresAnderson <allansoaresanderson@gmail.com>
2023-12-17 11:48:56 -0300

    Api Spring P$F

---
+++ src/main/java/br/com/psf/personalsystemfinance/PersonalsystemfinanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.psf.personalsystemfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalsystemfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalsystemfinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/psf/personalsystemfinance/PersonalsystemfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.psf.personalsystemfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalsystemfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
