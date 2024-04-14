80cb78aa7338009b6e6caf59d39b14a4432490fc
AllanSoaresAnderson <allansoaresanderson@gmail.com>
2024-04-14 12:46:44 -0300

    Logic Exception

---
+++ src/main/java/br/com/psf/personalsystemfinance/exceptions/LogicException.java
@@ -0,0 +1,9 @@
+package br.com.psf.personalsystemfinance.exceptions;
+
+public class LogicException extends  Exception{
+
+    public LogicException(String message){
+        super(message);
+    }
+
+}
